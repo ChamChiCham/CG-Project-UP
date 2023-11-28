abf85b463e3b192709c74ca17b42203f0ad1a399
KyeongWonMin(laptop) <sojoonghan2@naver.com>
2023-11-28 12:03:42 +0900

    기본적인 캐릭터 이동 구현

--- UP!!/Main.cpp
+++ UP!!/Main.cpp
@@ -85,12 +85,24 @@ private:
 	CPlayer player;
 	CMap* map_ptr = nullptr;
 	std::array<CMap, 3> maps;
-
+	
+	// 전역 변수
 	float d = 0.f;
 	float s = 0.f;
 	int current_map;
 	int moving_time;
-	bool moving;
+	bool moving_left;
+	bool moving_right;
+	bool moving_up;
+	bool moving_down;
+
+	typedef struct CurrentPos {
+		int xPos;
+		int yPos;
+		int zPos;
+		int way;
+	};
+	CurrentPos playerPos;
 public:
 
 	// --
@@ -153,16 +165,19 @@ public:
 		// --
 		light.pos = { 3.f, 0.f, 10.f };
 
-		view.eye = glm::vec3(3.0f, 4.0f, 10.0f);
-		view.at = glm::vec3(3.f, 4.f, 0.0f);
+		view.eye = glm::vec3(3.0f, 2.0f, 5.0f);
+		view.at = glm::vec3(3.f, 2.f, 0.0f);
 		view.up = glm::vec3(0.f, 1.f, 0.f);
 
-		player.getShape().scale(0, 0.03f, 0.03f, 0.03f);
-		player.getShape().translate(1, 0.f, 0.5f, 0.f);
+		// 초기 플레이어 위치/크기 조정
+		player.getShape().scale(0, 0.015f, 0.015f, 0.015f);
+		player.getShape().translate(1, 0.f, 0.5f, 0.1f);
 		player.getShape().rotate(2, 180.f, 0.f, 1.f, 0.f);
+		playerPos = { 0, 0, 0 };
 
 		proj = glm::perspective(glm::radians(60.f), static_cast<float>(WINDOW_SIZE_X) / static_cast<float>(WINDOW_SIZE_Y), 0.1f, 20.f);
 
+		// 초기 카메라 위치 조정
 		d = sqrtf(powf(view.eye.x - view.at.x, 2) + powf(view.eye.z - view.at.z, 2));
 		s = atan2f(view.eye.z - view.at.z, view.eye.x - view.at.x);
 		view.eye.x = view.at.x + d * cos(s + glm::radians(-20.f));
@@ -172,6 +187,10 @@ public:
 		// explain
 		// --
 
+		std::cout << "방향키: 플레이어 이동" << std::endl;
+		std::cout << "b: 스탠드 자세" << std::endl;
+		std::cout << "n: 홀드 자세" << std::endl;
+		std::cout << "m: 행 자세" << std::endl;
 		std::cout << "4/6: 카메라 x이동" << std::endl;
 		std::cout << "8/5: 카메라 y이동" << std::endl;
 		std::cout << "1/3: 카메라 z이동" << std::endl;
@@ -187,6 +206,7 @@ public:
 	// --
 	// define cb func
 	// --
+	enum {front, right, back, left};
 
 	void Display()
 	{
@@ -277,24 +297,6 @@ public:
 		case 'm':
 			player.changeStatus(PLAYER_HANG);
 			break;
-		case 'l':
-			player.getShape().translate(0, -1.f, 0.f, 0.f);
-			break;
-		case 'j':
-			player.getShape().translate(0, 1.f, 0.f, 0.f);
-			break;
-		case 'i':
-			player.getShape().translate(0, 0.f, 1.f, 0.f);
-			player.getShape().translate(0, 0.f, 0.f, 1.f);
-			view.eye.y += 0.1;
-			view.at.y += 0.1;
-			break;
-		case 'k':
-			player.getShape().translate(0, 0.f, -1.f, 0.f);
-			player.getShape().translate(0, 0.f, 0.f, -1.f);
-			view.eye.y -= 0.1;
-			view.at.y -= 0.1;
-			break;
 		}
 	}
 
@@ -305,7 +307,161 @@ public:
 
 	void SpecialKeys(const int _key, const int _x, const int _y)
 	{
+		switch (_key) {
+		case GLUT_KEY_UP:
+			// 이동중이 아니거나, 벤 조건에 만족하지 않으면 이동
+			if (moving_time == 0 && MoveUpBan() == false) {
+				player.changeStatus(PLAYER_HANG);
+				moving_up = true;
+				playerPos.yPos += 1;
+				playerPos.zPos -= 1;
+			}
+			// 이전에 바라보던 방향에 맞게 현재 바라보는 방향 수정
+			if (playerPos.way == right) {
+				player.getShape().rotate(1, 90.f, 0.f, 1.f, 0.f);
+			}
+			else if (playerPos.way == left) {
+				player.getShape().rotate(1, -90.f, 0.f, 1.f, 0.f);
+			}
+			// 현재 바라보는 방향 업데이트
+			playerPos.way = front;
+			break;
+		case GLUT_KEY_DOWN:
+			if (moving_time == 0 && MoveDownBan() == false) {
+				player.changeStatus(PLAYER_HANG);
+				moving_down = true;
+				playerPos.yPos -= 1;
+				playerPos.zPos += 1;
+			}
+			if (playerPos.way == right) {
+				player.getShape().rotate(1, 90.f, 0.f, 1.f, 0.f);
+			}
+			else if (playerPos.way == left) {
+				player.getShape().rotate(1, -90.f, 0.f, 1.f, 0.f);
+			}
+			playerPos.way = back;
+			break;
+		case GLUT_KEY_LEFT:
+			if (moving_time == 0 && MoveLeftBan() == false) {
+				moving_left = true;
+				playerPos.xPos -= 1;
+			}
+			if (playerPos.way == front || playerPos.way == back) {
+				player.getShape().rotate(1, 90.f, 0.f, 1.f, 0.f);
+			}
+			else if (playerPos.way == right) {
+				player.getShape().rotate(1, 180.f, 0.f, 1.f, 0.f);
+			}
+			playerPos.way = left;
+			break;
+		case GLUT_KEY_RIGHT:
+			if (moving_time == 0 && MoveRightBan() == false) {
+				moving_right = true;
+				playerPos.xPos += 1;
+			}
+			if (playerPos.way == front || playerPos.way == back) {
+				player.getShape().rotate(1, -90.f, 0.f, 1.f, 0.f);
+			}
+			else if (playerPos.way == left) {
+				player.getShape().rotate(1, 180.f, 0.f, 1.f, 0.f);
+			}
+			playerPos.way = right;
+			break;
+		}
+		std::cout << "xPos:" << playerPos.xPos << std::endl;
+		std::cout << "yPos:" << playerPos.yPos << std::endl;
+		std::cout << "zPos:" << playerPos.zPos << std::endl;
+		if (playerPos.way == 0) {
+			std::cout << "way: front" << std::endl;
+		}
+		else if (playerPos.way == 1) {
+			std::cout << "way: right" << std::endl;
+		}
+		else if (playerPos.way == 2) {
+			std::cout << "way: back" << std::endl;
+		}
+		else if (playerPos.way == 3) {
+			std::cout << "way: left" << std::endl;
+		}
+	}
 
+	void MovingUp()
+	{
+		if (moving_up) {
+			player.getShape().translate(3, 0.f, 0.1f, 0.f);
+			player.getShape().translate(3, 0.f, 0.f, -0.1f);
+			view.eye.y += 0.1f;
+			view.at.y += 0.1f;
+			EndMove();
+		}
+	}
+
+	void MovingDown()
+	{
+		if (moving_down) {
+			player.getShape().translate(3, 0.f, -0.1f, 0.f);
+			player.getShape().translate(3, 0.f, 0.f, 0.1f);
+			view.eye.y -= 0.1f;
+			view.at.y -= 0.1f;
+			EndMove();
+		}
+	}
+
+	void MovingLeft()
+	{
+		if (moving_left) {
+			player.getShape().translate(3, -0.1f, 0.f, 0.f);
+			EndMove();
+		}
+	}
+
+	void MovingRight()
+	{
+		if (moving_right) {
+			player.getShape().translate(3, 0.1f, 0.f, 0.f);
+			EndMove();
+		}
+	}
+
+	void EndMove()
+	{
+		moving_time++;
+		// 무빙 종료 조건
+		if (moving_time == 10) {
+			player.changeStatus(PLAYER_STAND);
+			moving_time = 0;
+			moving_right = false;
+			moving_left = false;
+			moving_up = false;
+			moving_down = false;
+		}
+	}
+
+	// 어떠한 조건을 충족하면 이동을 막음
+	bool MoveUpBan()
+	{
+		if (playerPos.way != front) {
+			return true;
+		}
+		return false;
+	}
+
+	bool MoveDownBan()
+	{
+		if (playerPos.yPos == 0) {
+			return true;
+		}
+		return false;
+	}
+
+	bool MoveLeftBan()
+	{
+		return false;
+	}
+
+	bool MoveRightBan()
+	{
+		return false;
 	}
 
 	// --
@@ -315,7 +471,10 @@ public:
 
 	void updateState()
 	{
-		
+		MovingUp();
+		MovingDown();
+		MovingLeft();
+		MovingRight();
 	}
 
 
